65c53e274d55df07efb969f4d8c48981d9792f40
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-12-08 23:37:58 +0700

    fix alias

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -21,9 +21,8 @@ module.exports = {
     alias: {
       assets: path.resolve(__dirname, 'src/assets'),
       '@': path.resolve(__dirname, '..', 'src'),
-      '@app': path.resolve(__dirname, '..', 'src/app'),
-      '@pages': path.resolve(__dirname, '..', 'src/app/pages'),
-      '@components': path.resolve(__dirname, '..', 'src/app/components'),
+      '@pages': path.resolve(__dirname, '..', 'src/pages'),
+      '@components': path.resolve(__dirname, '..', 'src/components'),
     },
   },
   stats: {
